d87864811ff1c3884e9421b97dbc7a1a3a6404dd
yoga setiawan <yogainformatika@gmail.com>
2020-01-19 17:14:06 +0700

    add DetailCourseViewModel

--- app/src/main/java/com/arioki/belajarandroidjetpackpro/ui/detail/DetailCourseActivity.kt
+++ app/src/main/java/com/arioki/belajarandroidjetpackpro/ui/detail/DetailCourseActivity.kt
@@ -3,11 +3,13 @@ package com.arioki.belajarandroidjetpackpro.ui.detail
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.arioki.belajarandroidjetpackpro.R
+import com.arioki.belajarandroidjetpackpro.data.CourseEntity
+import com.arioki.belajarandroidjetpackpro.data.ModuleEntity
 import com.arioki.belajarandroidjetpackpro.ui.render.CourseReaderActivity
-import com.arioki.belajarandroidjetpackpro.utils.DataDummy.generateDummyModules
 import com.arioki.belajarandroidjetpackpro.utils.DataDummy.getCourse
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -19,6 +21,8 @@ class DetailCourseActivity : AppCompatActivity() {
     companion object {
         const val EXTRA_COURSE = "extra_course"
         lateinit var adapter: DetailCourseAdapter
+        private lateinit var viewModel: DetailCourseViewModel
+        private lateinit var modules: List<ModuleEntity>
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,8 +30,10 @@ class DetailCourseActivity : AppCompatActivity() {
         setContentView(R.layout.activity_detail_course)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        adapter = DetailCourseAdapter()
 
+        viewModel = ViewModelProviders.of(this).get(DetailCourseViewModel::class.java)
+
+        adapter = DetailCourseAdapter()
         rv_module.isNestedScrollingEnabled = false
         rv_module.layoutManager = LinearLayoutManager(this)
         rv_module.setHasFixedSize(true)
@@ -39,15 +45,19 @@ class DetailCourseActivity : AppCompatActivity() {
         intent.extras?.let {
             val courseId = it.getString(EXTRA_COURSE)
             if (courseId != null) {
-                adapter.setModules(generateDummyModules(courseId))
-                populateCourse(courseId)
+                viewModel.setCourseId(courseId)
+                modules = viewModel.getModules()
+                adapter.setModules(modules)
             }
+        }
+        viewModel.getCourse()?.let {
+            populateCourse(viewModel.getCourse())
         }
 
     }
 
-    private fun populateCourse(courseId: String) {
-        val courseEntity = getCourse(courseId)
+    private fun populateCourse(courseEntity: CourseEntity) {
+        val courseEntity = getCourse(viewModel.getCourseId())
         courseEntity?.let { it ->
             text_title.text = it.title
             text_description.text = it.description
@@ -59,7 +69,7 @@ class DetailCourseActivity : AppCompatActivity() {
             btn_start.setOnClickListener {
                 val intent =
                     Intent(this, CourseReaderActivity::class.java)
-                intent.putExtra(CourseReaderActivity.EXTRA_COURSE_ID, courseId)
+                intent.putExtra(CourseReaderActivity.EXTRA_COURSE_ID, viewModel.getCourseId())
                 it.context.startActivity(intent)
             }
         }
--- app/src/main/java/com/arioki/belajarandroidjetpackpro/ui/detail/DetailCourseViewModel.kt
+++ app/src/main/java/com/arioki/belajarandroidjetpackpro/ui/detail/DetailCourseViewModel.kt
@@ -1,7 +1,35 @@
 package com.arioki.belajarandroidjetpackpro.ui.detail
 
 import androidx.lifecycle.ViewModel
+import com.arioki.belajarandroidjetpackpro.data.CourseEntity
+import com.arioki.belajarandroidjetpackpro.data.ModuleEntity
+import com.arioki.belajarandroidjetpackpro.utils.DataDummy.generateDummyCourses
+import com.arioki.belajarandroidjetpackpro.utils.DataDummy.generateDummyModules
+
 
 class DetailCourseViewModel : ViewModel() {
-    //TODO
+    private lateinit var mCourse: CourseEntity
+    private lateinit var courseId: String
+
+    fun getCourse(): CourseEntity {
+        for (i in generateDummyCourses().indices) {
+            val courseEntity = generateDummyCourses()[i]
+            if (courseEntity.courseId == courseId) {
+                mCourse = courseEntity
+            }
+        }
+        return mCourse
+    }
+
+    fun getModules(): List<ModuleEntity> {
+        return generateDummyModules(getCourseId())
+    }
+
+    fun setCourseId(courseId: String) {
+        this.courseId = courseId
+    }
+
+    fun getCourseId(): String {
+        return courseId
+    }
 }
