9038cd316ca585a0ed539a9c17bb64f5e5e5dce6
yoga setiawan <yogainformatika@gmail.com>
2020-01-19 10:44:30 +0700

    add AcademyAdapter

--- app/src/main/java/com/arioki/belajarandroidjetpackpro/academy/AcademyFragment.kt
+++ app/src/main/java/com/arioki/belajarandroidjetpackpro/academy/AcademyFragment.kt
@@ -2,25 +2,41 @@ package com.arioki.belajarandroidjetpackpro.academy
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.arioki.belajarandroidjetpackpro.AcademyAdapter
 import com.arioki.belajarandroidjetpackpro.R
+import com.arioki.belajarandroidjetpackpro.utils.DataDummy
+import kotlinx.android.synthetic.main.fragment_academy.*
+
 
 /**
  * A simple [Fragment] subclass.
  */
 class AcademyFragment : Fragment() {
+    lateinit var academyAdapter: AcademyAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_academy, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        if (activity != null) {
+            academyAdapter = AcademyAdapter(activity!!)
+            academyAdapter.setListCourses(DataDummy.generateDummyCourses())
+
+            rv_academy.layoutManager = LinearLayoutManager(activity)
+            rv_academy.setHasFixedSize(true)
+            rv_academy.adapter = academyAdapter
+
+        }
+    }
 
 }
