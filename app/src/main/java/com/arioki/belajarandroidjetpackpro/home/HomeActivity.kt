518b727d1c1ec6461c3c89cf165bc6fea540141f
yoga setiawan <yogainformatika@gmail.com>
2020-01-19 09:15:56 +0700

    Codelab Persiapan Asset

--- app/src/main/java/com/arioki/belajarandroidjetpackpro/home/HomeActivity.kt
+++ app/src/main/java/com/arioki/belajarandroidjetpackpro/home/HomeActivity.kt
@@ -21,9 +21,8 @@ class HomeActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home,
-                R.id.navigation_dashboard,
-                R.id.navigation_notifications
+                R.id.action_home,
+                R.id.action_bookmark
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
